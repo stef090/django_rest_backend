bbbcb073f52d39cc9941c3ed1027a9fde578c935
Stjepan Lovasić <stjepan.lovasic@intis.hr>
2017-12-19 11:29:06 +0100

    Added APIView

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View."""
+
+    def get(self, request, format=None):
+        """Return list of APIView features."""
+
+        api_view = [
+            'Use HTTP methods as functions(get, post, patch, put, delete)'
+        ]
+
+        return Response({
+                         'message':'Hello!',
+                         'api_view':api_view
+                         })
